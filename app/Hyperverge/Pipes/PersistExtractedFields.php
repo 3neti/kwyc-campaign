062ea0fe6f28d20c0c1a1826b47647c40f053a7a
jn-devops <devops@joy-nostalg.com>
2024-11-09 12:12:23 +0800

    tweak persist extracted fields to load extracted fields before counting

--- app/Hyperverge/Pipes/PersistExtractedFields.php
+++ app/Hyperverge/Pipes/PersistExtractedFields.php
@@ -12,8 +12,6 @@ class PersistExtractedFields
 {
     public function handle(Checkin $checkin, Closure $next)
     {
-//        dd(Arr::get($checkin->data, 'result.results.0.apiResponse.result.details.0'));
-//        dd(Arr::get($checkin->data, 'result.results.2.moduleId'));
         $details = Arr::get($checkin->data, Checkin::DATA_INDEX);
         foreach (Extracted::cases() as $extracted) {
             $field = $extracted->value;
@@ -21,7 +19,7 @@ class PersistExtractedFields
             if (!$value) continue;
             $checkin->extracted_fields()->create(compact('field', 'value'));
         }
-        ExtractedFieldsPersisted::dispatchIf($checkin->fresh()->extracted_fields->count() > 0, $checkin);
+        ExtractedFieldsPersisted::dispatchIf($checkin->load('extracted_fields')->extracted_fields->count() > 0, $checkin);
 
         return $next($checkin);
     }
